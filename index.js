8b3a8cb363261ceab9ca9e0ab68802c11f59595c
iinaamasum <iinaamasum@gmail.com>
2022-05-05 20:35:45 +0600

    api for user specific item deletion

--- index.js
+++ index.js
@@ -136,6 +136,13 @@ async function run() {
       const item = await UsersCollection.findOne(query);
       res.send(item);
     });
+
+    app.delete('/item/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await UsersCollection.deleteOne(query);
+      res.send(result);
+    });
   } finally {
   }
 }
